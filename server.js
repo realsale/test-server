fab75f00e2a3eeb6e866a1055bd1526d30cf1ad4
realsale <pwedesaken@gmail.com>
2019-10-17 11:52:32 +0800

    test not found

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.write("Hello World.");
-  res.end();
+  if ((req.url = "/")) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("Hello real.");
+    res.end();
+  } else {
+    res.writeHead(400, { "Content-Type": "text/html" });
+    res.end("404 Not Found");
+  }
 });
 
 server.listen(process.env.PORT || 4000, () => {
